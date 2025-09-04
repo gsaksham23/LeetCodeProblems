8523143880a7ccaf1ad6b0049d4a8043ef8bb3b3
Saksham Gupta <104646843+gsaksham23@users.noreply.github.com>
2025-09-04 19:49:30 +0530

    Time: 68 ms (16.72%) | Memory: 13.9 MB (92.92%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0); cout.tie(0);
         vector<int> ans(2);
         for(int i = 0; i < nums.size(); i++){
             int val = target - nums[i];
