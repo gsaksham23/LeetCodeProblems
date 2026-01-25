7331d7533fa2d8eef46868b834821a4e0ee997c5
Saksham Gupta <104646843+gsaksham23@users.noreply.github.com>
2026-01-26 00:07:51 +0530

    Time: 11 ms (47.42%) | Memory: 12 MB (41.79%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        // Fast I/O
+        ios_base::sync_with_stdio(0);
+        cin.tie(0); cout.tie(0);
+        // Code
         int left = 0, ans = 0;
         map<char, int> mp;
         for(int right = 0; right < s.size(); right++){
