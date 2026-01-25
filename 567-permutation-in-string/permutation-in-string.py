6ee3ffbff7cdf26e55ee929c6189bb143ecb60ae
Saksham Gupta <104646843+gsaksham23@users.noreply.github.com>
2026-01-26 01:44:24 +0530

    Time: 1090 ms (13.89%) | Memory: 19.5 MB (20.45%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,29 +1,16 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
-        count1, count2 = [0]*26, [0]*26
-        for i in range(len(s1)):
-            count1[ord(s1[i]) - ord('a')] += 1
-            count2[ord(s2[i]) - ord('a')] += 1
-        matches, l = 0, 0
-        for i in range(26):
-            matches += (1 if count1[i] == count2[i] else 0)
-        for r in range(len(s1), len(s2)):
-            if matches == 26:
-                return True
-            index = ord(s2[r]) - ord('a')
-            count2[index] += 1
-            if count1[index] == count2[index]:
-                matches += 1
-            elif count1[index] + 1 == count2[index]:
-                matches -= 1
-            
-            index = ord(s2[l]) - ord('a')
-            count2[index] -= 1
-            if count1[index] == count2[index]:
-                matches += 1
-            elif count1[index] - 1 == count2[index]:
-                matches -= 1
-            l += 1
-        return matches == 26
+          
+        left = 0
+        target = Counter(s1)
+        right = len(s1)-1
+
+        while right < len(s2):
+            window = Counter(s2[left:right+1])
+            if window == target:
+                return True 
+            right += 1
+            left += 1
+        return False
+
+# __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0")) 
